deff994c2e42d7a0b97e47e2af9ef5b0eeb53d98
Allen Geng <rugeng@microsoft.com>
2023-07-18 07:06:03 -0700

    added a placeholder for group chat page

--- simple_messaging/simpe-messaging-server.js
+++ simple_messaging/simpe-messaging-server.js
@@ -14,10 +14,26 @@ http.createServer(function (request, response) {
 
   if (url_parts.pathname == '/') {
     fs.readFile('./index.html', function (err, data) {
-      response.end(data)
+      response.end(data);
     });
+
+  } else if (url_parts.pathname.startsWith('/groupchat')) {
+    fs.readFile('./group-chat.html', function(err, data) {
+      response.end(data);
+    });
+
   } else if (url_parts.pathname.substr(0,5) == '/poll') {
     // polling code here
+    var count = url_parts.pathname.replace(/[^0-9]*/, '');
+    console.log(count);
+    if (messages.length > count) {
+      response.end(JSON.stringify({
+        count: messages.length, 
+        append: messages.slice(count).join('\n') + '\n'
+      }));
+    } else {
+      clients.push(response);
+    }
   }
 
 }).listen(8080, 'localhost')
